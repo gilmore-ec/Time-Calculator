09fc42e6ee464129c7dd011c61a8b8d4c034b026
Gilmore <144559541+gilmore-ec@users.noreply.github.com>
2024-06-15 18:06:45 -0400

    Created initial file with some libraries and a header for the main function.

---
+++ calculator.c
@@ -0,0 +1,20 @@
+/*
+Author: Timothy Gilmore (Gilmore-EC)
+Description: Pulls system time and recieves an amount of time from user and calculates the time when that amount of time has passed
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    for (int i = 0; i < argc; i++) {
+        //TODO - Process program's inputs
+    }
+
+    //TODO
+
+    return(0);
+}
